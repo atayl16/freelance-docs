fe62dd0948ba931adbb160011d1bba2d63982de6
atayl16 <atayl16@yahoo.com>
2021-01-04 15:42:22 +0100

    prepare for Heroku deploy2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://atayl16-freelance-documents.herokuapp.com/*'
+    origins 'https://atayl16-freelance-dashboard.herokuapp.com/*'
 
     resource '*',
       headers: :any,
